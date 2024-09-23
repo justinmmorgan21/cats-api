61d0ff6fd38abb974cdcd435ccfcff425a69dc68
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-22 21:13:30 -0500

    add index action

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,9 +1,22 @@
 class CatsController < ApplicationController
 
+  def cat_params = {
+    name: params[:name],
+    breed: params[:breed],
+    age: params[:age],
+    image: params[:image]
+  }
   def index
     @cats = Cat.all
     render :index
   end
 
-
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      render :show
+    else
+      render json: {error: @cat.errors.full_messages}, status: :unprocessale_entity
+    end
+  end
 end
