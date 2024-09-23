82a7ab54d7a84e415b1c50de09bb577cd256e053
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-22 20:16:16 -0500

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Cat.create(name: "Tiny", breed: "shorthair tabby", age: 2, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFi__LE6ZxsOxdoPLYNSXnCtt3gxu28u3zgQ&s");
+Cat.create(name: "Tyler", breed: "bengal mix", age: 1, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGbZGV_wCtX20I7z382kNNO3Wh7-POSbpWEA&s");
